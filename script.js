5a66f894fd881da6ff1bc68cad6cf1b4978920c3
Sascha Geprägs <gepraegssascha@gmail.com>
2025-05-14 12:25:52 +0200

    neue version!!

--- script.js
+++ script.js
@@ -3,14 +3,19 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
-let currentGames = ["devtest", 1111]
-gehtslos = false;
-currentPlayers = []
-var finishedPlayers = []
-var finishedPlayersTiming = []
+
+// Datenstruktur für Lobbys
+let lobbies = {
+    1111: {
+        players: [],
+        finishedPlayers: [],
+        finishedPlayersTiming: [],
+        gehtslos: false
+    }
+};
 
 app.get('/', (req, res) => {
-    res.send("<h1>Folgende Infos sind bekannt: </h1> <br> <p>Aktuelle Lobbys: </p>" + currentGames + "<br> <p>Geht's los?: </p>" + gehtslos + "<br> <p>Aktuelle Spieler: </p>" + currentPlayers + "<br><p>Liste der fertigen Spieler: </p>" + finishedPlayers + "<br><p>Zeit der gefinisheden Spieler: </p>" + finishedPlayersTiming + "<br><h1>Folgende APIs sind verfügbar: </h1> <br> <p>/gettest</p> <p>/posttest</p> <p>/checkForPlayer</p> <p>/registerLobby</p> <p>/gehtsLos</p> <p>/losGehts</p> <p>/binDa</p> <p>/finishCall</p> <p>/getFinishedPlayers</p> <p>/reset</p> <p>/getOpenLobbyList</p>");
+    res.send("<h1>Folgende Infos sind bekannt: </h1> <br> <p>Aktuelle Lobbys: </p>" + Object.keys(lobbies).join(", ") + "<br><h1>Folgende APIs sind verfügbar: </h1> <br> <p>/gettest</p> <p>/posttest</p> <p>/checkForPlayer</p> <p>/registerLobby</p> <p>/gehtsLos</p> <p>/losGehts</p> <p>/binDa</p> <p>/finishCall</p> <p>/getFinishedPlayers</p> <p>/reset</p> <p>/getOpenLobbyList</p>");
 });
 
 app.get('/gettest', (req, res) => {
@@ -18,64 +23,106 @@ app.get('/gettest', (req, res) => {
 });
 
 app.post('/posttest', (req, res) => {
-    var ergebnis = req.body.test
-    res.send("Danke, dass du " + ergebnis + " übermittelt hast!")
+    var ergebnis = req.body.test;
+    res.send("Danke, dass du " + ergebnis + " übermittelt hast!");
 });
 
 app.post('/checkForPlayer', (req, res) => {
-    if (currentPlayers.includes(req.body.username)) {
-        res.send(true)
+    const { lobby, username } = req.body;
+    if (lobbies[lobby] && lobbies[lobby].players.includes(username)) {
+        res.send(true);
     } else {
-        res.send(false)
+        res.send(false);
     }
-})
+});
 
 app.post('/registerLobby', (req, res) => {
-    currentGames.push(req.body.gamepin)
-    res.send(currentGames)
-})
+    const { gamepin } = req.body;
+    if (!lobbies[gamepin]) {
+        lobbies[gamepin] = {
+            players: [],
+            finishedPlayers: [],
+            finishedPlayersTiming: [],
+            gehtslos: false
+        };
+    }
+    res.send(Object.keys(lobbies));
+});
 
 app.get('/gehtsLos', (req, res) => {
-    if (gehtslos == true) {
-        res.send(gehtslos)
+    const { lobby } = req.query;
+    if (lobbies[lobby]) {
+        if (lobbies[lobby].gehtslos) {
+            res.send(true);
+        } else {
+            res.send(lobbies[lobby].players);
+        }
     } else {
-        res.send(currentPlayers)
+        res.status(404).send("Lobby nicht gefunden");
     }
-})
+});
 
 app.get('/losGehts', (req, res) => {
-    gehtslos = true;
-    res.send("oke");
-})
+    const { lobby } = req.query;
+    if (lobbies[lobby]) {
+        lobbies[lobby].gehtslos = true;
+        res.send("oke");
+    } else {
+        res.status(404).send("Lobby nicht gefunden");
+    }
+});
 
 app.post('/binDa', (req, res) => {
-    currentPlayers.push(req.body.username)
-    res.send(currentPlayers)
-})
+    const { lobby, username } = req.body;
+    if (lobbies[lobby]) {
+        if (!lobbies[lobby].players.includes(username)) {
+            lobbies[lobby].players.push(username);
+        }
+        res.send(lobbies[lobby].players);
+    } else {
+        res.status(404).send("Lobby nicht gefunden");
+    }
+});
 
 app.post('/finishCall', (req, res) => {
-    if (finishedPlayers.includes(req.body.username) == false) {
-        finishedPlayers.push(req.body.username)
-        finishedPlayersTiming.push(Math.floor(new Date().getTime() / 1000.0))
+    const { lobby, username } = req.body;
+    if (lobbies[lobby]) {
+        if (!lobbies[lobby].finishedPlayers.includes(username)) {
+            lobbies[lobby].finishedPlayers.push(username);
+            lobbies[lobby].finishedPlayersTiming.push(Math.floor(new Date().getTime() / 1000.0));
+        }
+        res.send(lobbies[lobby].finishedPlayers);
+    } else {
+        res.status(404).send("Lobby nicht gefunden");
     }
-    res.send(finishedPlayers)
-})
+});
 
 app.get('/getFinishedPlayers', (req, res) => {
-    res.send([finishedPlayers, finishedPlayersTiming])
-})
+    const { lobby } = req.query;
+    if (lobbies[lobby]) {
+        res.send([lobbies[lobby].finishedPlayers, lobbies[lobby].finishedPlayersTiming]);
+    } else {
+        res.status(404).send("Lobby nicht gefunden");
+    }
+});
 
 app.get('/getOpenLobbyList', (req, res) => {
-    res.send(currentGames)
-})
+    res.send(Object.keys(lobbies));
+});
 
 app.get('/reset', (req, res) => {
-    gehtslos = false;
-    currentPlayers = [];
-    finishedPlayers = []
-    finishedPlayersTiming = []
-    res.send("erfolgreich resettet");
-    currentGames = ["devtest"]
-})
+    const { lobby } = req.query;
+    if (lobbies[lobby]) {
+        lobbies[lobby] = {
+            players: [],
+            finishedPlayers: [],
+            finishedPlayersTiming: [],
+            gehtslos: false
+        };
+        res.send("Lobby erfolgreich resettet");
+    } else {
+        res.status(404).send("Lobby nicht gefunden");
+    }
+});
 
 app.listen(3000);
