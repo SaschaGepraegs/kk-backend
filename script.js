a87c8d3e949bd259ecd0cde782d2e4df008cb6f4
Sascha Geprägs <gepraegssascha@gmail.com>
2025-05-15 10:08:28 +0200

    biddeschee

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const Redis = require("ioredis");
 
 async function test() {
     try {
-        const client = new Redis("${process.env.REDIS_URL}");
+        const client = new Redis(process.env.REDIS_URL);
         await client.set("testKey", "hello");
         const value = await client.get("testKey");
         console.log("Wert aus Redis:", value);
